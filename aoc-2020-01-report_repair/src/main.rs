38789f425cd452a68a450bad194f3e3985bdd652
Vasilis Gerakaris <vasilis.gerakaris@navarino.gr>
2020-12-04 22:41:21 +0200

    add solution for problem 1b

--- aoc-2020-01-report_repair/src/main.rs
+++ aoc-2020-01-report_repair/src/main.rs
@@ -1,21 +1,39 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::error::Error;
+use std::fs::read_to_string;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input = File::open("files/input.txt")?;
-    let mut input_buffer = String::new();
     let mut numbers = HashSet::new();
+    let mut sums = HashMap::new();
+    let mut found = 0;
 
-    input.read_to_string(&mut input_buffer)?;
+    let input_buffer = read_to_string("files/input.txt")?;
     for line in input_buffer.as_str().lines() {
-        let number =  line.parse::<i32>()?;
-        if numbers.contains(&(2020 - number)) {
-            println!("Result: {}", number * (2020-number));
+        let number = line.parse::<i32>()?;
+        let diff = 2020 - number;
+
+        if numbers.contains(&diff) {
+            println!("Result (2 values): {}", number * diff);
+            found += 1;
+        }
+
+        if sums.contains_key(&diff) {
+            println!(
+                "Result (3 values): {}",
+                number * sums[&diff] * (2020 - number - sums[&diff])
+            );
+            found += 1;
+        } else {
+            for &num in &numbers {
+                sums.insert(num + number, num);
+            }
+        }
+
+        numbers.insert(number);
+        if found == 2 {
             return Ok(());
         }
-        numbers.insert(number);
     }
     return Ok(());
 }
