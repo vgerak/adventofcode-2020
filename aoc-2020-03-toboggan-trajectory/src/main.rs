c4ddbf29fb89a3905e0d69f11f1462ef89f6a563
Vasilis Gerakaris <vgerak@gmail.com>
2020-12-07 21:24:00 +0200

    add solution for problem 3

--- aoc-2020-03-toboggan-trajectory/src/main.rs
+++ aoc-2020-03-toboggan-trajectory/src/main.rs
@@ -8,29 +8,45 @@ enum MyError {
     Error,
 }
 
-fn main() -> Result<(), MyError> {
-    // let trees: Vec<Vec<char>>;
-    let input_buffer = read_to_string("files/input.txt").map_err(MyError::Io)?;
-    // for line in input_buffer.as_str().lines() {
-    //     let mut foo = line.chars().collect::<Vec<char>>();
-    //     trees.append(&foo);
-    // }
-    let matrix: Vec<Vec<char>> = input_buffer.lines().map(|l| l.chars().collect()).collect();
-    let mut cur_col = 0;
-    // let mut tree_map: HashMap<(i32, i32), (i32, i32)> = ((1, 1), (3, 1), (5, 1), (7, 1), (1,2))
-    //     .map(|x, y| (0, 0))
-    //     .collect();
+#[derive(Debug)]
+struct State {
+    trees: u64,
+    column: usize
+}
 
+#[derive(Hash, PartialEq, Eq, Debug)]
+struct Path {
+    down: usize,
+    right: usize
+}
+
+// impl std::hash::Hash for Path { };
+
+fn main() -> Result<(), MyError> {
+    let input_buffer = read_to_string("files/input.txt").map_err(MyError::Io)?;
+
+    let matrix: Vec<Vec<char>> = input_buffer.lines().map(|l| l.chars().collect()).collect();
     let row_len = matrix.first().ok_or(MyError::Error)?.len();
 
-    let mut tree_count = 0;
-    for row in matrix {
-        if row[cur_col] == '#' {
-            tree_count += 1;
+    // let mut state = State{trees: 0, column: 0};
+    let mut tree_map = HashMap::new();
+    tree_map.insert(Path{down: 1, right: 1}, State{trees: 0, column: 0});
+    tree_map.insert(Path{down: 1, right: 3}, State{trees: 0, column: 0});
+    tree_map.insert(Path{down: 1, right: 5}, State{trees: 0, column: 0});
+    tree_map.insert(Path{down: 1, right: 7}, State{trees: 0, column: 0});
+    tree_map.insert(Path{down: 2, right: 1}, State{trees: 0, column: 0});
+
+    for (i, row) in matrix.iter().enumerate() {
+        for (path, state) in tree_map.iter_mut() {
+            if i % path.down == 0 {
+                if row[state.column] == '#' {
+                    state.trees += 1;
+                }
+                state.column = (state.column + path.right) % row_len;
+            }
         }
-        cur_col = (cur_col + 3) % row_len;
     }
-    println!("Result 1: {}", tree_count);
-    // println!("Result 2: {}", cnt2);
+    println!("Result 1: {}", tree_map[&Path{down: 1, right: 3}].trees);
+    println!("Result 2: {}", tree_map.values().fold(1 as u64, |prod, x| prod * x.trees));
     return Ok(());
 }
