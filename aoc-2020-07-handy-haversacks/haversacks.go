db0443c5e00f8d9cfaa3a9f032d01d0adbc62d3b
Vasilis Gerakaris <vgerak@gmail.com>
2020-12-13 23:57:48 +0200

    add solution for problem 7b

--- aoc-2020-07-handy-haversacks/haversacks.go
+++ aoc-2020-07-handy-haversacks/haversacks.go
@@ -17,44 +17,63 @@ func splitRule(s, sep string) (string, string) {
 	return x[0], x[1]
 }
 
-func recFindContainers(color string, rules map[string][]containedBag, containers map[string]bool) {
-	if containingBags, ok := rules[color]; ok {
-		// fmt.Println("FOUND", color, ":", containingBags)
-		for _, bag := range containingBags {
-			containers[bag.Color] = true
-			recFindContainers(bag.Color, rules, containers)
+func recFindContainers(color string, rulesContained map[string][]string, containers map[string]bool) {
+	if containingBags, ok := rulesContained[color]; ok {
+		for _, bagColor := range containingBags {
+			containers[bagColor] = true
+			recFindContainers(bagColor, rulesContained, containers)
 		}
 	}
 }
 
+func recCountContained(color string, rulesContains map[string][]containedBag, bagCount map[string]int) (count int) {
+	count = 1
+	if numBags, ok := bagCount[color]; ok {
+		return numBags
+	}
+	if containedBags, ok := rulesContains[color]; ok {
+		for _, bag := range containedBags {
+			var numBags = recCountContained(bag.Color, rulesContains, bagCount)
+			bagCount[bag.Color] = numBags
+			count += bag.Num * numBags
+		}
+	}
+	// fmt.Printf("color %q adds %d bags\n", color, count)
+	return
+}
+
 func main() {
 	dat, _ := ioutil.ReadFile("input.txt")
-	// dat, _ := ioutil.ReadFile("input.txt")
-	var rules = make(map[string][]containedBag)
+	// dat, _ := ioutil.ReadFile("testinput.txt")
+	// dat, _ := ioutil.ReadFile("testinput2.txt")
+	var rulesContains = make(map[string][]containedBag)
+	var rulesContained = make(map[string][]string)
 	for _, rule := range strings.Split(string(dat), "\n") {
 		if rule == "" {
 			break
 		}
 		colorPart, containedPart := splitRule(rule, " bags contain ")
+		rulesContains[colorPart] = make([]containedBag, 0)
 		for _, containedBagStr := range strings.Split(containedPart, ", ") {
 			numStr, containedColorPart := splitRule(containedBagStr, " ")
 			var containedColor = strings.TrimSpace(strings.TrimRight(containedColorPart, "bags."))
 			if num, err := strconv.Atoi(numStr); err == nil {
-				var bag = containedBag{Color: colorPart, Num: num}
-				if _, ok := rules[containedColor]; ok {
-					rules[containedColor] = append(rules[containedColor], bag)
+				var bag = containedBag{Color: containedColor, Num: num}
+				rulesContains[colorPart] = append(rulesContains[colorPart], bag)
 
+				if _, ok := rulesContained[containedColor]; ok {
+					rulesContained[containedColor] = append(rulesContained[containedColor], colorPart)
 				} else {
-					rules[containedColor] = make([]containedBag, 1)
-					rules[containedColor][0] = bag
+					rulesContained[containedColor] = make([]string, 1)
+					rulesContained[containedColor][0] = colorPart
 				}
 			}
 		}
 	}
-
 	var containers = make(map[string]bool)
-	recFindContainers("shiny gold", rules, containers)
+	recFindContainers("shiny gold", rulesContained, containers)
 
+	var bagCount = make(map[string]int)
 	fmt.Println("Result1:", len(containers))
-	// fmt.Println("Result2:", res2)
+	fmt.Println("Result2:", recCountContained("shiny gold", rulesContains, bagCount)-1)
 }
